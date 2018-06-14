fa2f96ebb764d29efc8150948b135013fd7063cb
Justin Perkins <perkinsjj1@gmail.com>
2018-06-14 12:48:01 -0500

    broken test example

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,10 @@
 describe('page', () => {
-  it('works', () => {})
+  it('works', () => {
+
+  cy.visit('https://www.google.com/')
+
+  cy.get('input#lst-ib.gsfi').type('cypress testing {enter}')
+
+  cy.get('[data-hveid="38"] > .rc > .r > a').click()
+  })
 })
